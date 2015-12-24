9a75e705b62dc3cefdacc348a4f459eab17c8ba7
Will Clark <wclark3@gmail.com>
2015-12-24 07:25:09 -0600

    works for arbitrary n

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,17 @@
 $(function() {
 
-	var num_boxes = 2;
+	var num_boxes = 5;
+	for (var i = 0; i < num_boxes; i++) {
+	    var input_img = "<div class='row input-img'>" +
+	    "<div class='col-md-6'>" + 
+	    "<input type='text' class='form-control' id='tex" + i + "' placeholder='y_t = a_t k_t^{\alpha} n_t^{1-\alpha}'>" + 
+        "</div>" +
+        "<div class='col-md-6'>" +
+        "<div><img id='svg" + i + "' src=''></div>" +
+        "</div>" +
+        "</div>"
+        $("#input-img-holder").append(input_img);
+	}
 
 	$("#btn-submit").click(function() {
 		for (var i = 0; i < num_boxes; i++) {
